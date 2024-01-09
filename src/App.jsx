59517a6fe31baba243a55ecd1f2e339d0f6bf217
Josjay05 <ojoshjay@gmail.com>
2024-01-09 16:38:41 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+import {
+	CustomerReviews,
+	Footer,
+	Hero,
+	PopularProducts,
+	SpecialOffer,
+	SuperQuality,
+	Services,
+	Subscribe,
+} from "./Sections";
+const App = () => {
+	// const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<main className="relative">
+				<section className="x1:padding-1 wide:padding-r padding-b">
+					<Hero />
+				</section>
+				<section className="padding">
+					<PopularProducts />
+				</section>
+				<section className="padding">
+					<SuperQuality />
+				</section>
+				<section className="padding">
+					<Services />
+				</section>
+				<section className="padding-x py-10">
+					<SpecialOffer />
+				</section>
+				<section className="bg-pale-blue padding">
+					<CustomerReviews />
+				</section>
+				<section className="padding-x sm:py-32 py-16 w-full">
+					<Subscribe />
+				</section>
+				<section className="padding bg-blackpadding-x padding-t pb-8">
+					<Footer />
+				</section>
+			</main>
+		</>
+	);
+};
+
+export default App;
---
+++ src/Sections/Nav.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import logo from "../assets/favicon.ico";
+const Nav = () => {
+	return (
+		<header>
+			<nav>
+				<img src={logo} alt="logo" width={130} height={29} />
+			</nav>
+		</header>
+	);
+};
+
+export default Nav;
